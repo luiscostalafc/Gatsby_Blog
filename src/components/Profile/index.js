21ff18b241e24868562e407d472fdda45a7712f1
luiscostalafc <luiscostalafc@gmail.com>
2020-04-15 17:30:46 -0300

    Iniciando com GraphQL

---
+++ src/components/Profile/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Profile = () => (
+  <div className="Profile-wrapper">
+    <h1>Matt Foo</h1>
+    <h2>Programador Full Stack</h2>
+    <p> Description ....</p>
+  </div>
+)
+
+export default Profile;
