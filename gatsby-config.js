a84a35f7a6ffa8f91f8cccc342879bf431a8c105
luiscostalafc <luiscostalafc@gmail.com>
2020-04-16 15:20:24 -0300

    Estilizando primeiros componentes

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,6 +6,7 @@ module.exports = {
     author: `@Luis_Costa`,
   },
   plugins: [
+    `gatsby-plugin-styled-components`,
     `gatsby-plugin-react-helmet`,
     {
       resolve: `gatsby-source-filesystem`,
