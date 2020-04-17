d3675d691fee3856ce7028949aac287388a6a0af
luiscostalafc <luiscostalafc@gmail.com>
2020-04-16 22:21:58 -0300

    Criando e estilizando os Menus

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import PropTypes from "prop-types";
 import Sidebar from '../Sidebar';
 import * as S from "./styles";
+import MenuBar from '../MenuBar';
 
 import GlobalStyles from '../../styles/global';
 
@@ -11,6 +12,7 @@ const Layout = ({ children }) => {
       <GlobalStyles />
    <Sidebar/>
     <S.LayoutMain>{children}</S.LayoutMain>
+    <MenuBar />
     </S.LayoutWrapper>
   )
  
