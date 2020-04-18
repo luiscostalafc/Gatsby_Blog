37ba9b9941ff7227ab9afd82df39de857fb261a9
luiscostalafc <luiscostalafc@gmail.com>
2020-04-17 21:02:03 -0300

    Adicionando fields com o onCreateNode

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,24 +1,61 @@
 import React from "react";
+import { useStaticQuery, graphql } from "gatsby";
 
 import Layout from "../components/Layout";
 import SEO from "../components/seo";
 import PostItem from '../components/PostItem';
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
+const IndexPage = () => {
+  const { allMarkdownRemark } = useStaticQuery(
+    graphql`
+    query PostList {
+      allMarkdownRemark {
+        edges {
+          node {
+            fields {
+              slug
+            }
+            frontmatter {
+              background
+              category
+              date(locale: "pt-br", formatString: "DD [de] MMMM [de] YYYY")
+              description
+              title
+            }
+            timeToRead
+          }
+        }
+      }
+    }
+
+   `)
+
+
+   const postList = allMarkdownRemark.edges
+
+  return (
+    <Layout>
+      <SEO title="Home" />
+      {postList.map(({node: { frontmatter: { background, category, date, description, title },
+      timeToRead,
+      fields: { slug }
+      },
+      }) => (
     <PostItem
-      slug="/about/"
-      background="blue"
-      category="Misc"
-      date="16 de Abril de 2020"
-      timeToRead="5"
-      title="O título..."
-      description="A descrição...."
-    
-    />
-     
-  </Layout>
-)
+        slug={slug}
+        background={background}
+        category={category}
+        date={date}
+        timeToRead={timeToRead}
+        title={title}
+        description={description}
+      />
+      ))}
+
+    </Layout>
+  )
+
+
+}
 
 export default IndexPage
